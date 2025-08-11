e3607b5bf6d7d2bd01eccbbc6794849c265ff5f4
Heike Stein <heike.c.stein@gmail.com>
2025-08-11 10:54:06 +0200

    Update _approximate_mll.py
    pass weights to KL

--- gpytorch/mlls/_approximate_mll.py
+++ gpytorch/mlls/_approximate_mll.py
@@ -56,7 +56,7 @@ class _ApproximateMarginalLogLikelihood(MarginalLogLikelihood, ABC):
         # Get likelihood term and KL term
         num_batch = approximate_dist_f.event_shape[0]
         log_likelihood = self._log_likelihood_term(approximate_dist_f, target, **kwargs).div(num_batch)
-        kl_divergence = self.model.variational_strategy.kl_divergence().div(self.num_data / self.beta)
+        kl_divergence = self.model.variational_strategy.kl_divergence(**kwargs).div(self.num_data / self.beta)
 
         # Add any additional registered loss terms
         added_loss = torch.zeros_like(log_likelihood)
